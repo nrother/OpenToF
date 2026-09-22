92506b800ddb8e58325221736ace473e3a7838a5
Niklas Rother <info@niklas-rother.de>
2026-09-22 20:06:04 +0200

    Also plot the debugdata

--- software/visualizer/plot.py
+++ software/visualizer/plot.py
@@ -4,6 +4,8 @@ import sys
 df = pd.read_csv(sys.argv[1])
 df_gyro = df[["gx","gy","gz"]]
 df_accel = df[["ax","ay","az"]]
+df_fifolevel = df[["debugdata"]]
 df_gyro.plot()
 df_accel.plot()
+df_fifolevel.plot()
 plt.show()
