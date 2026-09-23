29f2327d5762de80c15d9d121a3ea98831a359ac
Niklas Rother <info@niklas-rother.de>
2026-09-23 18:09:00 +0200

    Update plotting script

--- software/visualizer/plot.py
+++ software/visualizer/plot.py
@@ -1,11 +1,17 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import sys
+
+fig, ax = plt.subplots(3,1, figsize=(20,10))
+
 df = pd.read_csv(sys.argv[1])
+
 df_gyro = df[["gx","gy","gz"]]
 df_accel = df[["ax","ay","az"]]
 df_fifolevel = df[["debugdata"]]
-df_gyro.plot()
-df_accel.plot()
-df_fifolevel.plot()
+
+df_gyro.plot(ax=ax[0], title="Gyro")
+df_accel.plot(ax=ax[1], title="Accel")
+df_fifolevel.plot(ax=ax[2], title="Debugdata")
+
 plt.show()
